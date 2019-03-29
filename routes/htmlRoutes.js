452abfaa830a7ab82ddb2eb91186c61f6698742e
WHBJ <bevansrealtors@gmail.com>
2019-03-29 00:13:19 -0500

    working html routes and html pages moved per express layout

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,21 @@
-var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
-  // Load index page
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  // If no matching route is found default to home
+  app.get("/results", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/visitor.html"));
   });
 };
