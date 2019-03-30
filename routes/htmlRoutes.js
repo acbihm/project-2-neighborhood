f611be2cbefe45892cbb1d25ca60c4397efd528b
Olinda73 <olinda73@me.com>
2019-03-30 12:58:49 -0500

    changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,17 +15,6 @@ module.exports = function(app) {
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
-<<<<<<< HEAD
-  app.get("/newUser", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/newUser.html"));
-  });
-  app.get("/signIn", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/signIn.html"));
-  });
-  // If no matching route is found default to home
-=======
-
->>>>>>> df51283881d27f98ae9a79468c6e4eae7fa2fee4
   app.get("/results", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/visitor.html"));
   });
