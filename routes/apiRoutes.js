83cd7e654bbd8d551ac214a51744475fc42600cc
Alex Bihm <43941153+acbihm@users.noreply.github.com>
2019-04-02 22:58:08 -0500

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -113,6 +113,7 @@ module.exports = function (app) {
   //   // res.status(204).end();
   // });
 
+
   // app.post('/api/answer', function (req, res) {
   //   db.answers.create(req.body).then(function (dbAnswer) {
   //     res.json(dbAnswer);
@@ -128,4 +129,11 @@ module.exports = function (app) {
   //     res.json(dbExample);
   //   });
   // });
+    app.delete("/api/answer/kill", function (req, res) {
+    db.answers.destroy({ where: { user_name: "" } }).then(function(
+      dbAnswer
+    ) {
+      res.json(dbAnswer);
+    });
+  
 };
